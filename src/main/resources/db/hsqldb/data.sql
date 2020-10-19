d41e58d2b51c8d044d68f3beeee8d3df29e205f9
canjorbus <candelajordano@gmail.com>
2020-10-19 19:07:15 +0200

    ejercicio 8

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -68,7 +68,6 @@ INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (14, 'Lala', '2019-
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (16, 'Anzai', '2019-07-13', 3, 13);
 
 
-
 INSERT INTO visits(id,pet_id,visit_date,description) VALUES (1, 7, '2013-01-01', 'rabies shot');
 INSERT INTO visits(id,pet_id,visit_date,description) VALUES (2, 8, '2013-01-02', 'rabies shot');
 INSERT INTO visits(id,pet_id,visit_date,description) VALUES (3, 8, '2013-01-03', 'neutered');
