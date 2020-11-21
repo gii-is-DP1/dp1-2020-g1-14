3bccc4922d9f9254b34588a9a27cf9cffbb10c3d
Usuario <Usuario@DESKTOP-UMAJ9EI>
2020-11-21 21:32:52 +0100

    Añadido un par de restricciones en el formulario de Restaurantes

--- src/main/java/org/springframework/samples/petclinic/model/Restaurante.java
+++ src/main/java/org/springframework/samples/petclinic/model/Restaurante.java
@@ -2,18 +2,21 @@ package org.springframework.samples.petclinic.model;
 
 import javax.persistence.Entity;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 import lombok.Data;
 
 @Data
 @Entity
 public class Restaurante extends NamedEntity {
-    
+    @NotNull
 	private String tipo;
-   
+    @NotNull
 	private String localizacion;
-   
+    @Positive
     private Integer aforomax;
-    
+    @Positive
     private Integer aforores;
 
 }
