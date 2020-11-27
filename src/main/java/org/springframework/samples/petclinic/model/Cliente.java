7474eef443e546316f38f904f3ddd5a9cb20366b
Jaime Ramos <j23rl07@gmail.com>
2020-11-27 19:22:08 +0100

    Entidad Propietario y relación propietario-restaurantes

--- src/main/java/org/springframework/samples/petclinic/model/Cliente.java
+++ src/main/java/org/springframework/samples/petclinic/model/Cliente.java
@@ -1,26 +1,21 @@
 package org.springframework.samples.petclinic.model;
 
 import javax.persistence.Entity;
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
-
 import com.sun.istack.NotNull;
-
 import lombok.Data;
 
 @Data
 @Entity
 public class Cliente extends Usuario {
+	
     @NotNull
-    @NotEmpty
     private Boolean esSocio;
     
-    @NotNull
-    @NotEmpty(message="El número de teléfono es obligatorio.")
+    @NotBlank(message="El número de teléfono es obligatorio.")
     @Pattern(regexp="^([0-9]|7[1-9])[0-9]{8}$", message="Debe introducir un número de teléfono válido p.ej: '954678970', en caso de teléfono móvil: '657908756'.")
     private String tlf;
     
-    @NotNull
-    @NotEmpty
-    private Integer numPedidos;
+    private int numPedidos;
 }
