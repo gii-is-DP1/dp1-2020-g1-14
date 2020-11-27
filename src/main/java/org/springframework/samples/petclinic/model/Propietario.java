c7a0f783af6dcbf3b86f9ec2f594635c403a7928
Usuario <Usuario@DESKTOP-UMAJ9EI>
2020-11-27 21:00:45 +0100

    Añadida una batería de funciones

--- src/main/java/org/springframework/samples/petclinic/model/Propietario.java
+++ src/main/java/org/springframework/samples/petclinic/model/Propietario.java
@@ -1,5 +1,9 @@
 package org.springframework.samples.petclinic.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -8,9 +12,10 @@ import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
-import lombok.Data;
+import org.springframework.beans.support.MutableSortDefinition;
+import org.springframework.beans.support.PropertyComparator;
 
-@Data
+
 @Entity
 public class Propietario extends Usuario{
 	
@@ -20,5 +25,63 @@ public class Propietario extends Usuario{
 	
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "propietario")
 	private Set<Restaurante> restaurantes;
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public void setRestaurantes(Set<Restaurante> restaurantes) {
+		this.restaurantes = restaurantes;
+	}
+	
+	protected Set<Restaurante> getRestaurantesInternal() {
+		if (this.restaurantes == null) {
+			this.restaurantes = new HashSet<>();
+		}
+		return this.restaurantes;
+	}
+
+	protected void setRestaurantesInternal(Set<Restaurante> restaurantes) {
+		this.restaurantes = restaurantes;
+	}
+	
+	public List<Restaurante> getRestaurantes() {
+		List<Restaurante> sortedRestaurantes = new ArrayList<>(getRestaurantesInternal());
+		PropertyComparator.sort(sortedRestaurantes, new MutableSortDefinition("name", true, true));
+		return Collections.unmodifiableList(sortedRestaurantes);
+	}
+	
+	public void addRestaruante(Restaurante restaurante) {
+		getRestaurantesInternal().add(restaurante);
+		restaurante.setPropietario(this);
+	}
+	
+	public boolean removeRestaurantes(Restaurante restaurante) {
+		return getRestaurantesInternal().remove(restaurante);
+	}
+	
+	public Restaurante getRestaurante(String name) {
+		return getRestaurante(name, false);
+	}
+
+	private Restaurante getRestaurante(String name, boolean ignoreNew) {
+		name = name.toLowerCase();
+		for(Restaurante restaurante : getRestaurantesInternal()) {
+			if(!ignoreNew || !restaurante.isNew()) {
+				String resName = restaurante.getName();
+				resName = resName.toLowerCase();
+				if(resName.equals(name)) {
+					return restaurante;
+				}
+			}
+		}
+		return null;
+	}
+	
+	
 
 }
--- src/main/java/org/springframework/samples/petclinic/model/Restaurante.java
+++ src/main/java/org/springframework/samples/petclinic/model/Restaurante.java
@@ -3,24 +3,55 @@ package org.springframework.samples.petclinic.model;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 
-import lombok.Data;
-
-@Data
 @Entity
 public class Restaurante extends NamedEntity {
     @NotNull
 	private String tipo;
-    @NotNull
+    @NotBlank
 	private String localizacion;
     @Positive
-    private Integer aforomax;
+    private int aforomax;
     @Positive
-    private Integer aforores;
+    private int aforores;
     @ManyToOne
     @JoinColumn(name = "propietario_id")
     private Propietario propietario;
+	
+    public String getTipo() {
+		return tipo;
+	}
+	public String getLocalizacion() {
+		return localizacion;
+	}
+	public int getAforomax() {
+		return aforomax;
+	}
+	public int getAforores() {
+		return aforores;
+	}
+	public Propietario getPropietario() {
+		return propietario;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public void setLocalizacion(String localizacion) {
+		this.localizacion = localizacion;
+	}
+	public void setAforomax(int aforomax) {
+		this.aforomax = aforomax;
+	}
+	public void setAforores(int aforores) {
+		this.aforores = aforores;
+		//aqui se podria hacer una resta entre el maximo y el numero de clientes con reserva
+	}
+	public void setPropietario(Propietario propietario) {
+		this.propietario = propietario;
+	}
+    
 
 }
