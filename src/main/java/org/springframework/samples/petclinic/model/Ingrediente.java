d4bb4fdd104de682d07f47e5b1601b3518a3f7ca
Hegoa <hegoaria@gmail.com>
2020-11-28 17:53:47 +0100

    Relaciones entre ingrediente y producto e ingrediente y restaurante

--- src/main/java/org/springframework/samples/petclinic/model/Ingrediente.java
+++ src/main/java/org/springframework/samples/petclinic/model/Ingrediente.java
@@ -1,17 +1,63 @@
 package org.springframework.samples.petclinic.model;
 
+import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.PositiveOrZero;
 
-import lombok.Data;
-
-@Data
 @Entity
 public class Ingrediente extends NamedEntity {
 
-	private Double stock;
+	@PositiveOrZero
+	private double stock;
 	
 	@Enumerated(EnumType.STRING)
 	private Medida medida;
+
+	@ManyToOne
+	@JoinColumn(name = "restaurante_id")
+	private Restaurante restaurante;
+	
+	@ManyToMany
+	@JoinColumn(name = "producto_id")
+	private Set<Producto> productos;
+	
+	public double getStock() {
+		return stock;
+	}
+
+	public void setStock(double stock) {
+		this.stock = stock;
+	}
+
+	public Medida getMedida() {
+		return medida;
+	}
+
+	public void setMedida(Medida medida) {
+		this.medida = medida;
+	}
+
+	public Restaurante getRestaurante() {
+		return restaurante;
+	}
+
+	public void setRestaurante(Restaurante restaurante) {
+		this.restaurante = restaurante;
+	}
+
+	public Set<Producto> getProductos() {
+		return productos;
+	}
+
+	public void setProductos(Set<Producto> productos) {
+		this.productos = productos;
+	}
+
+	
 }
