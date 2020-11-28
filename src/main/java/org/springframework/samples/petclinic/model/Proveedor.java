274e5a69a65ddc955fb768608cd9dc2142326e2b
canjorbus <candelajordano@gmail.com>
2020-11-28 18:23:10 +0100

    He añadido crear reclamación, junto a las pruebas unitarias y la relación de reclamación con restaurante.

--- src/main/java/org/springframework/samples/petclinic/model/Proveedor.java
+++ src/main/java/org/springframework/samples/petclinic/model/Proveedor.java
@@ -1,10 +1,8 @@
 package org.springframework.samples.petclinic.model;
 
 import javax.persistence.Entity;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 
-import com.sun.istack.NotNull;
 
 import lombok.Data;
 
@@ -13,8 +11,7 @@ import lombok.Data;
 
 
 public class Proveedor extends NamedEntity{
-    @NotNull
-    @NotEmpty(message="El número de teléfono es obligatorio.")
+   
     @Pattern(regexp="^([0-9]|7[1-9])[0-9]{8}$", message="Debe introducir un número de teléfono válido p.ej: '954678970', o en caso de teléfono móvil: '657908756'.")
 	private String tlf;
     
--- src/main/java/org/springframework/samples/petclinic/model/Restaurante.java
+++ src/main/java/org/springframework/samples/petclinic/model/Restaurante.java
@@ -1,8 +1,12 @@
 package org.springframework.samples.petclinic.model;
 
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
@@ -21,6 +25,9 @@ public class Restaurante extends NamedEntity {
     @JoinColumn(name = "propietario_id")
     private Propietario propietario;
 	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "restaurante")
+	private Set<Reclamacion> reclamaciones;
+    
     public String getTipo() {
 		return tipo;
 	}
