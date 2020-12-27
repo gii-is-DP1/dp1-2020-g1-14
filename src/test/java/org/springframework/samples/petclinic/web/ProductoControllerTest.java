91e7cae4f70c2c2c4f59c5d8de95130a8bd8e311
Badayco <badarhndz@gmail.com>
2020-12-27 10:30:17 +0000

    Añadidos más Controller Tests en Producto

--- src/test/java/org/springframework/samples/petclinic/web/ProductoControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/web/ProductoControllerTest.java
@@ -63,12 +63,53 @@ public class ProductoControllerTest {
 		given(this.productoService.findProductoById(TEST_PRODUCTO_ID)).willReturn(Optional.of(pizza));
 		
 }
+	//INSERT TESTS
+	
 	@WithMockUser(value = "spring")
     @Test
     void testInitCreationForm() throws Exception {
 	mockMvc.perform(get("/productos/new")).andExpect(status().isOk()).andExpect(model().attributeExists("producto"))
 			.andExpect(view().name("productos/editProducto"));
 }
+	
+/*
+	@WithMockUser(value = "spring")
+	@Test
+	void testProcessCreationFormSuccess() throws Exception{
+		mockMvc.perform(post("/productos")
+				.param("name", "Macarrones con Queso")
+				.param("alergenos", "Queso")
+				.with(csrf())
+				.param("precio", "7.70"))
+				.andExpect(status().is3xxRedirection());
+	}
+*/	
+	
+	
+	@WithMockUser(value = "spring")
+	@Test
+	void testProcessCreationFormHasErrors() throws Exception{
+		mockMvc.perform(post("/productos/save")
+				.param("name", "Macarrones con Queso")
+				.param("alergenos", "Queso")
+				.with(csrf())
+				.param("precio", "fgrthfdthrty5465464y4hggrt4ghr"))
+		.andExpect(status().isOk())
+		.andExpect(model().attributeHasErrors("producto"))
+		.andExpect(model().attributeHasFieldErrors("producto", "precio"))
+		.andExpect(view().name("productos/editProducto"));
+	}
+	
+	
+	//Test listar productos
+	@WithMockUser(value = "spring")
+    @Test
+    void testListadoProducto() throws Exception {
+		mockMvc.perform(get("/productos")).andExpect(status().isOk()).andExpect(model().attributeExists("productos"))
+		.andExpect(status().is2xxSuccessful()).andExpect(view().name("productos/listadoProductos"));
+	}
+	
+	//UPDATE TESTS
 	
 	@WithMockUser(value = "spring")
 	@Test
@@ -82,27 +123,35 @@ public class ProductoControllerTest {
 }
 	
 
-				
-	@WithMockUser(value = "spring")
-    @Test
-    void testProcessFindFormSuccess() throws Exception {
-		given(this.productoService.findProductoByName("")).willReturn(Lists.newArrayList(pizza, new Producto()));
+	
+		@WithMockUser(value = "spring")
+		@Test
+		void testProcessUpdateProductoFormSuccess() throws Exception {
+			mockMvc.perform(post("/productos/{productoId}/edit", TEST_PRODUCTO_ID)
+								.with(csrf())
+								.param("name", "Pizza")
+								.param("alergenos", "Leche")
+								.param("precio", "6.50"))
+								.andExpect(status().is3xxRedirection())
+					.andExpect(view().name("redirect:/productos/{productoId}"));
+		}
 
-		mockMvc.perform(get("/productos")).andExpect(status().isOk()).andExpect(view().name("productos/listadoProductos"));
-}
-		
-		
-	
-
-
 	
 	
 	
-	
-	
-	
-	
-	
+		@WithMockUser(value = "spring")
+		@Test
+		void testProcessUpdateProductoFormHasErrors() throws Exception{
+			mockMvc.perform(post("/productos/{productoId}/edit", TEST_PRODUCTO_ID)
+					.with(csrf())
+					.param("name", "Pizza")
+					.param("alergenos", "Leche")
+					.param("precio", "jsdfhusdhfusdfnvcudshfhryuhvnvb"))
+			.andExpect(status().isOk())
+			.andExpect(model().attributeHasErrors("producto"))
+			.andExpect(model().attributeHasFieldErrors("producto", "precio"))
+			.andExpect(view().name("productos/editProducto"));
+		}
 	
 	
 	
