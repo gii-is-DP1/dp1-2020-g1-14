c6b04a071f352fef010bfc8ccd9020a2a3348e44
Badayco <badarhndz@gmail.com>
2021-01-09 10:43:35 +0000

    Implementados tests del controller de LineaPedido y Pedido

--- src/test/java/org/springframework/samples/petclinic/web/LineaPedidoControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/web/LineaPedidoControllerTest.java
@@ -1,7 +1,6 @@
 package org.springframework.samples.petclinic.web;
 
 import static org.hamcrest.Matchers.hasProperty;
-
 import static org.hamcrest.Matchers.is;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -12,7 +11,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -23,97 +21,150 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.samples.petclinic.configuration.SecurityConfiguration;
-import org.springframework.samples.petclinic.model.Cliente;
 import org.springframework.samples.petclinic.model.Estado;
 import org.springframework.samples.petclinic.model.LineaPedido;
 import org.springframework.samples.petclinic.model.Pedido;
-import org.springframework.samples.petclinic.model.Pet;
-import org.springframework.samples.petclinic.service.ClienteService;
+import org.springframework.samples.petclinic.model.Producto;
+import org.springframework.samples.petclinic.service.LineaPedidoService;
 import org.springframework.samples.petclinic.service.PedidoService;
-import org.springframework.samples.petclinic.service.VetService;
+import org.springframework.samples.petclinic.service.ProductoService;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(controllers=PedidoController.class,
+@WebMvcTest(controllers = LineaPedidoController.class,
 excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class),
 excludeAutoConfiguration= SecurityConfiguration.class)
 
-public class PedidoControllerTest {
 
-	private static final int TEST_PEDIDO_ID = 1;
-	private static final int TEST_CLIENTE_ID = 1;
+public class LineaPedidoControllerTest {
+
+private static final int TEST_LINEAPEDIDO_ID = 1;
+private static final int TEST_PEDIDO_ID = 1;
+private static final int TEST_PRODUCTO_ID = 1;
 	
 	@Autowired
-	private PedidoController pedidoController;
+	private LineaPedidoController lineaPedidoController;
 
+	@MockBean
+	private LineaPedidoService lineaPedidoService;
+	
 	@MockBean
 	private PedidoService pedidoService;
 	
 	@MockBean
-	private ClienteService clienteService;
+	private ProductoService productoService;
 
 	@Autowired
 	private MockMvc mockMvc;
 	
-	private Cliente juan;
+	private LineaPedido lineaPedido;
 	private Pedido ped1;
+	private Producto tarta;
 	
-
 	@BeforeEach
 	void setup() {
-	
-		juan = new Cliente();
-		juan.setEsSocio(true);
-		juan.setId(TEST_CLIENTE_ID);
-		juan.setName("Juan");
-		juan.setNumPedidos(12);
-		juan.setPassword("pass1");
-		juan.setrDate(LocalDate.of(2000, 10, 11));
-		juan.setTlf("954765812");
 		
 		Pedido ped1 = new Pedido();
 		ped1.setAdress("Calle A");
-		ped1.setCliente(juan);
 		ped1.setEstado(Estado.PROCESANDO);
 		ped1.setId(TEST_PEDIDO_ID);
 		ped1.setOrderDate(LocalDate.of(2020, 8, 13));
 		ped1.setPrice(17.3);
 		
-		given(this.pedidoService.findPedidoById(TEST_PEDIDO_ID)).willReturn(Optional.of(ped1));
-		given(this.clienteService.findClienteById(TEST_CLIENTE_ID)).willReturn(Optional.of(juan));
+		tarta = new Producto();
+		tarta.setAlergenos("Lacteos, Huevo y Gluten");
+		tarta.setId(TEST_PRODUCTO_ID);
+		tarta.setName("Tarta");
+		tarta.setPrecio(6.0);
 		
-	}
-	
+		lineaPedido = new LineaPedido();
+		lineaPedido.setCantidad(2);
+		lineaPedido.setId(TEST_LINEAPEDIDO_ID);
+		lineaPedido.setPedido(ped1);
+		lineaPedido.setProducto(tarta);
+		
+		given(this.productoService.findProductoById(TEST_PRODUCTO_ID)).willReturn(Optional.of(tarta));
+		given(this.pedidoService.findPedidoById(TEST_PEDIDO_ID)).willReturn(Optional.of(ped1));
+		given(this.lineaPedidoService.findLineaPedidoById(TEST_LINEAPEDIDO_ID)).willReturn(Optional.of(lineaPedido));
+
+
+		
+		
+}
 	//CREATION TESTS
 	
 	@WithMockUser(value = "spring")
     @Test
     void testInitCreationForm() throws Exception {
-		mockMvc.perform(get("/pedidos/new", TEST_PEDIDO_ID)).andExpect(status().isOk())
-			.andExpect(view().name("pedidos/nuevoPedido"));
+	mockMvc.perform(get("/lineaPedidos/new")).andExpect(status().isOk()).andExpect(model().attributeExists("lineaPedido"))
+			.andExpect(view().name("lineaPedidos/editLineaPedido"));
 }
-
+	
+	
+	
 	@WithMockUser(value = "spring")
     @Test
     void testProcessCreationFormSuccess() throws Exception {
-		mockMvc.perform(post("/pedidos/new")
-						.with(csrf())
-						.param("adress", "Calle X")
-						.param("orderDate", "2020-12-27")
-						.param("price", "18.3"))
+		mockMvc.perform(post("/lineaPedidos/new").with(csrf()).param("producto", "Macarrones con Queso").param("cantidad", "2"))
 			.andExpect(status().is2xxSuccessful());
 }
-
-		
 	
-	//TEST LISTAR PEDIDOS
+
+	//TEST LISTAR LINEAPEDIDOS
 		@WithMockUser(value = "spring")
 	    @Test
-	    void testListadoPedidos() throws Exception {
-			mockMvc.perform(get("/pedidos")).andExpect(status().isOk()).andExpect(model().attributeExists("pedidos"))
-			.andExpect(status().is2xxSuccessful()).andExpect(view().name("pedidos/listadoPedidos"));
+	    void testListadoLineaPedido() throws Exception {
+			mockMvc.perform(get("/lineaPedidos")).andExpect(status().isOk()).andExpect(model().attributeExists("lineaPedidos"))
+			.andExpect(status().is2xxSuccessful()).andExpect(view().name("lineaPedidos/listadoLineaPedidos"));
 		}
+	
+	
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
