c994493ce2992dac0be85f76b333c0b225a77d89
canjorbus <candelajordano@gmail.com>
2021-02-05 13:48:36 +0100

    Añadir logs y cambiar nombre de query

--- src/main/java/org/springframework/samples/petclinic/service/GerenteService.java
+++ src/main/java/org/springframework/samples/petclinic/service/GerenteService.java
@@ -2,11 +2,14 @@ package org.springframework.samples.petclinic.service;
 
 import java.util.Optional;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.model.Gerente;
 import org.springframework.samples.petclinic.repository.GerenteRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import ch.qos.logback.classic.Logger;
 
 @Service
 public class GerenteService {
@@ -17,39 +20,49 @@ public class GerenteService {
 	private AuthoritiesService authoritiesService;
 	@Autowired
 	private UserService userService;
+	
+	private static final Logger log = (Logger) LoggerFactory.getLogger(GerenteService.class);
+	
 
 	   @Transactional 
 	    public int gerenteCount() {
+		   log.info("Contando el número total de elementos");
 	        return (int) gerenteRepo.count();
 	    }
 	    @Transactional
 	    public Iterable<Gerente> findAll() {
+	    	log.info("Buscando todos los elementos");
 	        return gerenteRepo.findAll();
 	        
 	    }
 	    
 	    @Transactional(readOnly=true)
 	    public Optional<Gerente> findGerenteById(int id) {
+	    	log.info("Devolviendo elemento por su id");
 	        return gerenteRepo.findById(id);
 	    }
 	    
 	    @Transactional(readOnly = true)
 		public Optional<Gerente> findGerenteByUsuario(String username) {
-
-			return gerenteRepo.findClienteByUsuario(username);
+	    	log.info("Devolviendo elemento por su usuario");
+			return gerenteRepo.findGerenteByUsuario(username);
 		}
 	    
 	    @Transactional
 	    public void save(Gerente gerente) {
+	    	log.info("Guardando elemento");
 	    	gerenteRepo.save(gerente);
-	    	//crear usuario
+	    	
+	    	log.info("Creando usuario");
 			userService.saveUser(gerente.getUser());
-	    	//crear authorities
+			
+			log.info("Creando authorities");
 			authoritiesService.saveAuthorities(gerente.getUser().getUsername(), "gerente");
 	    }
 	    
 	    @Transactional
 	    public void delete(Gerente gerente) {
+	    	log.info("Eliminado un elemento");
 	    	userService.delete(gerente.getUser());
 	    	gerenteRepo.delete(gerente);
 	    }
