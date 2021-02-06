f228daa0298d9ac7829ccc9d1650aeb317440d66
Usuario <Usuario@DESKTOP-UMAJ9EI>
2021-02-06 19:25:41 +0100

    Eliminado de los Versions innecesarios

--- src/main/java/org/springframework/samples/petclinic/model/LineaPedido.java
+++ src/main/java/org/springframework/samples/petclinic/model/LineaPedido.java
@@ -3,7 +3,6 @@ package org.springframework.samples.petclinic.model;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.Version;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
@@ -14,17 +13,6 @@ import lombok.Setter;
 @Entity
 @Table(name="lineaPedido")
 public class LineaPedido extends BaseEntity {
-	
-	@Version
-	private Integer version;
-
-	public Integer getVersion() {
-		return version;
-	}
-
-	public void setVersion(Integer version) {
-		this.version = version;
-	}
 	
 	@NotNull(message="Introduce una cantidad mayor o igual a 1")
 	@Min(value=1,message="La cantidad debe de ser mayor o igual a 1")
